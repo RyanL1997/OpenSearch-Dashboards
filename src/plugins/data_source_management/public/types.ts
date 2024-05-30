cbf1163a59dc2d24f142ad2784f33a8caf880376
Ryan Liang <jiallian@amazon.com>
2024-05-30 16:19:50 -0700

    Setup creation panel draft 0
    Signed-off-by: Ryan Liang <jiallian@amazon.com>

--- src/plugins/data_source_management/public/types.ts
+++ src/plugins/data_source_management/public/types.ts
@@ -145,3 +145,5 @@ export {
 export type DatasourceType = 'S3GLUE' | 'PROMETHEUS';
 
 export type DatasourceStatus = 'ACTIVE' | 'DISABLED';
+
+export type AuthMethod = 'noauth' | 'basicauth' | 'awssigv4';
